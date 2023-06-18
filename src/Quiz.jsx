b0d285676d98b8efbec95251f189d4588ce639af
Isaac Castillo <isaac.castillo@berkeley.edu>
2023-06-18 13:08:48 -0700

    combined exrcises with conversation

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,7 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const supportedLanguages = [
+  { value: 'english', label: 'English' },
+  { value: 'french', label: 'French' },
+  { value: 'german', label: 'German' },
+  { value: 'spanish', label: 'Spanish' },
+  { value: 'italian', label: 'Italian' },
+  { value: 'portuguese', label: 'Portuguese' },
+  // Add more supported languages here
+];
 
 export default function Quiz() {
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [selectedTheme, setSelectedTheme] = useState('');
+  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+  
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+  
+  const handleThemeChange = (event) => {
+    setSelectedTheme(event.target.value);
+  };
+  
+  const handleDifficultyChange = (event) => {
+    setSelectedDifficulty(event.target.value);
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Call GPT-4 API or perform language learning operations
+  };
+  
   return (
-    <div>Quiz</div>
-  )
+    <div className="container">
+      <h1 className="mt-5 text-center">Vocabella</h1>
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-8 col-md-6">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group mb-3">
+              <label>Select a Language:</label>
+              <select
+                className="form-control form-control-sm"
+                value={selectedLanguage}
+                onChange={handleLanguageChange}
+              >
+                {supportedLanguages.map((language) => (
+                  <option key={language.value} value={language.value}>
+                    {language.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group mb-3">
+              <label>Enter a Theme:</label>
+              <input
+                type="text"
+                className="form-control form-control-sm"
+                value={selectedTheme}
+                onChange={handleThemeChange}
+              />
+            </div>
+            <div className="form-group mb-3">
+              <label>Select Difficulty Level:</label>
+              <select
+                className="form-control form-control-sm"
+                value={selectedDifficulty}
+                onChange={handleDifficultyChange}
+              >
+                <option value="beginner">Beginner</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="advanced">Advanced</option>
+                {/* Add more difficulty options */}
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary text-center mx-auto d-block">
+              Start Learning
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/utils/openAIAPIMethods.js
+++ src/utils/openAIAPIMethods.js
@@ -1,11 +1,12 @@
 const { Configuration, OpenAIApi } = require("openai");
 
+const configuration = new Configuration({
+	apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export const sendOpenAICall = async (prompt) => {
 	try {
-		const configuration = new Configuration({
-			apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-		});
-		const openai = new OpenAIApi(configuration);
 		const result = await openai.createCompletion({
 			model: "text-davinci-003",
 			prompt: prompt,
@@ -17,4 +18,50 @@ export const sendOpenAICall = async (prompt) => {
 		console.error('open ai call failed: ', e);
 		return undefined;
 	}	
+}
+
+export const getVocabWords = async (theme, language, difficulty) => {
+	try {
+		const result = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: generateVocabPrompt(theme, language, difficulty),
+			temperature: 0.5,
+			max_tokens: 4000,
+		});
+		return result?.data?.choices?.[0].text
+	} catch (e) {
+		console.error('open ai call failed: ', e);
+		return undefined;
+	}	 
+}
+
+function generateVocabPrompt(theme, language, difficulty) {
+	return `Generate 20 vocabulary words in ${language}
+	for the following theme: ${theme}
+	
+	Use the following format for your response:
+	{word in ${language}} - {word in english}
+	
+	Select words for the following proficiency level: ${difficulty}`;
+}
+
+export const getReadingExercise = async (words, language, difficulty) => {
+	try {
+		const result = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: generateReadingPrompt(words, language, difficulty),
+			temperature: 0.5,
+			max_tokens: 4000,
+		});
+		return result?.data?.choices?.[0].text
+	} catch (e) {
+		console.error('open ai call failed: ', e);
+		return undefined;
+	}	 
+}
+
+function generateReadingPrompt(words, language, difficulty) {
+	return `Generate a paragraph using the following words:
+	${words}
+	In ${language} for the following language proficiency: ${difficulty}`;
 }
